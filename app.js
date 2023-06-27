f95805b53d6940ee2a22a0853a05574e99a7a2a6
Amrutha-avidi <avidiamruthavalli@gmail.com>
2023-06-27 04:40:49 +0000

    express instance error

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const app = express();
+app.use(express.json());
+const dbPath = path.join(__dirname, "twitterClone.db");
+let db = null;
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000");
+    });
+  } catch (e) {
+    console.log(`DBError:${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+//1.
+app.post("/register/", async (request, response) => {
+  const { username, password, name, gender } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser !== undefined) {
+    response.status(400);
+    response.send("User already exists");
+  } else {
+    if (password.length < 6) {
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const createUserQuery = `
+      INSERT INTO user (username, password, name,  gender) 
+      VALUES ('${username}', '${hashedPassword}', '${name}','${gender}');`;
+
+      const dbResponse = await db.run(createUserQuery);
+      response.status(200);
+      response.send("User created successfully");
+    }
+  }
+});
+module.exports = app;
